440c736f7e3392bc0e980e5454b621e63577b374
davidlongbyu <42119997+davidlongbyu@users.noreply.github.com>
2026-01-19 10:09:15 -0800

    Update solutions.js
    Fixing how this loads (maybe?)

--- solutions.js
+++ solutions.js
@@ -2,14 +2,15 @@
 // Loads the NYT Wordle solution list from a known-good repo
 
 window.SOLUTIONS = [];
-
-fetch("https://raw.githubusercontent.com/tabatkins/wordle-list/main/answers.txt")
+window.SOLUTIONS_LOADED = fetch(
+  "https://raw.githubusercontent.com/tabatkins/wordle-list/main/answers.txt"
+)
   .then(r => r.text())
   .then(text => {
     window.SOLUTIONS = text
       .split("\n")
       .map(w => w.trim())
-      .filter(w => w.length === 5);
+      .filter(w => w.length===5);
     console.log(`Loaded ${SOLUTIONS.length} solutions`);
   })
   .catch(err => {
